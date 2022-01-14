2b3a508050141a9a7408852125554a8869950262
brwong3 <brwong3@s.sfusd.edu>
2022-01-14 00:23:28 -0800

    Fixed Code so passes PR Tests

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,6 +1,5 @@
 package frc.robot;
 
-
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -10,38 +9,32 @@ import frc.robot.Auto.SecondAuto;
 import frc.robot.Auto.ThirdAuto;
 import frc.robot.Auto.ZeroAuto;
 import frc.robot.commands.Drive;
-import frc.robot.constants.JoystickConstants;
 import frc.robot.subsystems.Drivetrain;
 
-
 public class RobotContainer {
-  
+
   private Drivetrain drivetrain = new Drivetrain();
 
-  private Joystick leftJoystick = new Joystick(JoystickConstants.leftJoystickPort);
-  private Joystick rightJoystick = new Joystick(JoystickConstants.rightJoystickPort);
-  private Joystick secondaryJoystick = new Joystick(JoystickConstants.secondaryJoystickPort);
+  private Joystick leftJoystick = new Joystick(Constants.JoystickConstants.leftJoystickPort);
+  private Joystick rightJoystick = new Joystick(Constants.JoystickConstants.rightJoystickPort);
+  private Joystick secondaryJoystick = new Joystick(Constants.JoystickConstants.secondaryJoystickPort);
 
-  //TODO:REMOVE ALL EXAMPLE BUTTONS, COMMANDS, METHODS AFTER WEEK 1
+  // TODO:REMOVE ALL EXAMPLE BUTTONS, COMMANDS, METHODS AFTER WEEK 1
 
-  //Buttons
+  // Buttons
   private final JoystickButton exampleButton = new JoystickButton(secondaryJoystick, 1);
 
-
-  //Commands
+  // Commands
   private final Drive drive = new Drive(drivetrain, leftJoystick, rightJoystick);
   private final Drive exampleCommand = new Drive(drivetrain, leftJoystick, rightJoystick);
-  
 
-  //Auto
+  // Auto
   private final ZeroAuto zeroAuto = new ZeroAuto();
   private final FirstAuto firstAuto = new FirstAuto();
   private final SecondAuto secondAuto = new SecondAuto();
   private final ThirdAuto thirdAuto = new ThirdAuto();
 
   private final SendableChooser<Command> sendableChooser = new SendableChooser<>();
-
-  
 
   public RobotContainer() {
     // Configure the button bindings
@@ -51,11 +44,11 @@ public class RobotContainer {
 
   private void configureButtonBindings() {
     drivetrain.setDefaultCommand(drive);
-    exampleButton.whenHeld(exampleCommand); //Example Code 
+    exampleButton.whenHeld(exampleCommand); // Example Code
   }
 
   private void configureAutos() {
-    //TODO: Rename Autos on Dashboard
+    // TODO: Rename Autos on Dashboard
 
     sendableChooser.setDefaultOption("No Auto", zeroAuto);
     sendableChooser.addOption("First Auto", firstAuto);
@@ -63,7 +56,7 @@ public class RobotContainer {
     sendableChooser.addOption("Third Auto", thirdAuto);
   }
 
-  public Command getAutonomousCommand() { 
+  public Command getAutonomousCommand() {
 
     return sendableChooser.getSelected();
   }
--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -2,12 +2,11 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
-
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.constants.CanIds;
+import frc.robot.Constants;
 
-public class Drivetrain extends SubsystemBase{
+public class Drivetrain extends SubsystemBase {
     private WPI_TalonFX rightFrontTalon;
     private WPI_TalonFX rightRearTalon;
     private WPI_TalonFX leftFrontTalon;
@@ -16,10 +15,10 @@ public class Drivetrain extends SubsystemBase{
     private MotorControllerGroup rightMotors;
 
     public Drivetrain() {
-        rightFrontTalon = new WPI_TalonFX(CanIds.rightFrontTalon);
-        rightRearTalon = new WPI_TalonFX(CanIds.rightRearTalon);
-        leftFrontTalon = new WPI_TalonFX(CanIds.leftFrontTalon);
-        leftRearTalon = new WPI_TalonFX(CanIds.leftRearTalon);
+        rightFrontTalon = new WPI_TalonFX(Constants.CanIds.rightFrontTalon);
+        rightRearTalon = new WPI_TalonFX(Constants.CanIds.rightRearTalon);
+        leftFrontTalon = new WPI_TalonFX(Constants.CanIds.leftFrontTalon);
+        leftRearTalon = new WPI_TalonFX(Constants.CanIds.leftRearTalon);
 
         leftMotors = new MotorControllerGroup(leftFrontTalon, leftRearTalon);
         rightMotors = new MotorControllerGroup(rightFrontTalon, rightRearTalon);
