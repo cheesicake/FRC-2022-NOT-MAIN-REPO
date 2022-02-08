a7193013041b949561b428efabc19f79880aecb3
Ethan Hanlon <ehanlon19@gmail.com>
2022-02-07 18:28:57 -0800

    Revert "Revert "Cleaned Up Auto Files""
    This reverts commit 3a45aa6cbc21c13e790d4fd40e212e0c51904498.

---
+++ src/main/java/frc/robot/auto/SecondAuto.java
@@ -0,0 +1,7 @@
+package frc.robot.auto;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class SecondAuto extends CommandBase{
+    
+}
---
+++ src/main/java/frc/robot/auto/ZeroAuto.java
@@ -0,0 +1,8 @@
+package frc.robot.auto;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+//Does Nothing
+public class ZeroAuto extends CommandBase{
+    
+}
