5becf34dbb803e441b51c3c5768eb1221b66414b
Machii21 <pazajoshua@gmail.com>
2022-02-20 15:38:23 -0800

    rough auto

--- src/main/java/frc/robot/auto/FirstAuto.java
+++ src/main/java/frc/robot/auto/FirstAuto.java
@@ -22,30 +22,68 @@ public class FirstAuto extends ParallelCommandGroup {
     Intake intake;
     Feeder feeder;
     Shooter shooter;
+    Neck neck;
     Trajectories trajectories = new Trajectories();
     Trajectory trajectory;
 
 
 
-    public FirstAuto(Drivetrain drivetrain, Arm arm, Intake intake, Feeder feeder, Shooter shooter) {
+    public FirstAuto(Drivetrain drivetrain, Arm arm, Intake intake, Feeder feeder, Shooter shooter, Neck neck) {
         this.drivetrain = drivetrain;
         this.arm = arm;
         this.intake = intake;
         this.feeder = feeder;
         this.shooter = shooter;
-        trajectory =  trajectories.loadTrajectory("Path");
+        this.neck = neck;
+        trajectory =  trajectories.loadTrajectory("path1");
 
         new ParallelCommandGroup(
           trajectories.followTrajectory(drivetrain, trajectory),
-          new SequentialCommandGroup(
-              new Shoot(shooter),
-              new WaitCommand(5),
-              new ArmAndIntake(arm, intake).withTimeout(5),
-              new RunFeeder(feeder, Direction.FORWARDS),
-              new WaitCommand(5)
-          )  
-        ).withTimeout(15);
+          new SequentialCommandGroup( 
+              new ParallelCommandGroup( //initial shoot
+                new WaitCommand(1),
+                new shoot(shooter) //assuming ball in the neck
+              ).withTimeout(1);
 
+              new SequentialCommandGroup( //take ball 2
+                new moveArm(arm, ArmState.LOW),
+                new runFeeder(feeder, Direction.FORWARDS),
+                new moveArm(arm, ArmState.HIGH),
+                new runNeck(neck, Direction.FORWARDS)
+              ).withTimeout(1);
 
+              new SequentialCommandGroup( //take ball 3
+                new moveArm(arm, ArmState.LOW),
+                new runFeeder(feeder, Direction.FORWARDS),
+                new moveArm(arm, ArmState.HIGH)
+                //new runNeck(neck, Direction.FORWARDS)
+              ).withTimeout(1);
+
+              new SequentialCommandGroup( //shoot balls 2 and 3
+                new shoot(shooter),
+                new runNeck(neck, Direction.FORWARDS),
+                new shoot(shooter)
+              ).withTimeout(1);
+
+              // >>>> ambitious mode == on >:) <<<<
+              new SequentialCommandGroup( //take ball 1
+                new moveArm(arm, ArmState.LOW),
+                new runFeeder(feeder, Direction.FORWARDS),
+                new moveArm(arm, ArmState.HIGH),
+                new runNeck(neck, Direction.FORWARDS)
+              ).withTimeout(1);
+
+              new SequentialCommandGroup( //take ball 4
+                new moveArm(arm, ArmState.LOW),
+                new runFeeder(feeder, Direction.FORWARDS),
+                new moveArm(arm, ArmState.HIGH)
+              ).withTimeout(1);
+
+              new SequentialCommandGroup( //shoot balls 1 and 4
+                new shoot(shooter),
+                new runNeck(neck, Direction.FORWARDS),
+                new shoot(shooter)
+              ).withTimeout(1);
+          ).withTimeout(15);
     }
 }
